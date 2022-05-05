0466e447f50e53bb9a3b4a664cfd65023ca5a7ef
Sandra <slopez@cifpfbmoll.eu>
2022-05-05 16:23:00 +0200

    lectura archivo json y csv (falta poder leer si hay arrays)

--- src/main/java/biblioteca/com/configuration/PruebaCSV.java
+++ src/main/java/biblioteca/com/configuration/PruebaCSV.java
@@ -1,5 +1,16 @@
 package biblioteca.com.configuration;
 
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+
 /**
  *
  * @author David
@@ -10,6 +21,14 @@ package biblioteca.com.configuration;
 public class PruebaCSV {
     
     public void leerCSV(){
-        
+        File csvFile = new File("D:\\DAM2\\config\\numeros.csv");
+        CsvSchema schema = CsvSchema.emptySchema().withHeader();
+        try{
+        ObjectReader objectReader = new CsvMapper().readerFor(User.class).with(schema);
+        MappingIterator<User> userMappingIterator = objectReader.readValues(csvFile);
+            System.out.println(userMappingIterator.readAll());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
